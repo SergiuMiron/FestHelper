a3e3101797161abfc11840f998086a9804cb06d5
Sergiu Miron <Sergiu.Miron@endava.com>
2019-01-13 22:57:26 +0200

    Implement the details page for a location (needs improvement)

--- my-app/src/modules/location_details/locationDetails.jsx
+++ my-app/src/modules/location_details/locationDetails.jsx
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { getLocations } from '../locations/apiCalls';
 import { locations } from '../../endpoints';
 import { Grid,Col,Image,Row } from 'react-bootstrap';
+import Moment from 'react-moment';
+import Button  from '../../common/button/button';
 import './locationDetails.scss';
 
 class LocationDetails extends Component {
@@ -41,17 +43,57 @@ class LocationDetails extends Component {
     render() {
         return(
             <Fragment>
-                <Grid>
-                    <Row className="left-container">
-                        <Col md={6} mdPull={6}>
-                            <Image src={this.state.pictures}></Image>
+                <div className="big-container">
+                    <div className="upper-container__details-page">
+                        <div className="left-container__details-page">
+                            <Image src={this.state.pictures.length > 0 ? this.state.pictures : "/assets/no-image.png"} ></Image>
                             <div className="container-phone-number">
-                                <i class="fas fa-phone"></i>
-                                <span>0{this.state.phone}</span>
+                                <i className="fas fa-phone"></i>
+                                <div className="phone-number-block">
+                                    <span>0{this.state.phone}</span>
+                                    <span>Mobile</span>
+                                </div>
                             </div>
-                        </Col>
-                    </Row>
-                </Grid>
+                        </div>
+
+                        <div className="right-container__details-page">
+                            <span className="details-location title">{this.state.name}</span>
+                            <div className="details-location price">
+                                ${this.state.price}.00
+                                <span>(per night)</span>
+                            </div>
+                            <div className="details-location">
+                                Located in:
+                            </div>
+                            <div className="city">{this.state.city}</div>
+
+                            <div className="details-location">
+                                Available in the next date interval: 
+                            </div>
+                            <Moment format="MM.DD.YYYY">{this.state.startLocation}</Moment>
+                            <span>  -  </span>
+                            <Moment format="MM.DD.YYYY">{this.state.endLocation}</Moment>
+                        </div>
+                    </div>
+
+                    <div className="bottom-container__details-page">
+                        <div className="details-location">
+                            Description
+                            <div className="description-content">{this.state.description}</div>
+                        </div>
+
+                    </div>
+                    
+                    <div className="details-location-button align-right">
+                        <Button action={this.addToWishlist}
+                                title="Add to my wishlist"
+                                size="Medium"></Button>
+                  </div>
+
+
+
+                </div>
+
             </Fragment>
         )
     }
